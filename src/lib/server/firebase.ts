554ef68e4e6df486b5b218a3b42a8ac4fa541b81
Alessio Toscano <alessio.toscano@innovery.net>
2023-04-25 18:05:19 +0200

    Remove JSON import for firebase credential in favor of  an environment variable

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -1,9 +1,9 @@
+import { FIREBASE_CONFIG } from '$env/static/private';
 import Firebase from 'firebase-admin';
-import account from './PRIVATE_firebase_private.json';
 import { getFirestore } from 'firebase-admin/firestore';
 
 export const adminFirebaseApp = Firebase.initializeApp({
-    credential: Firebase.credential.cert(account as Firebase.ServiceAccount)
+    credential: Firebase.credential.cert(JSON.parse(FIREBASE_CONFIG) as Firebase.ServiceAccount)
 })
 
 export const database = getFirestore(adminFirebaseApp);
